828890a1b7ccd8120b26fcad7d8e0def9c03923f
bates <bates@trabilisim.com>
2019-09-16 21:42:07 +0300

    assets

---
+++ assets/js/base.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $("#preloader").fadeOut(500, function () { $(this).remove(); Animate(); })    function Animate() {
+        $("#card").animate({
+            bottom: '10%',
+            opacity: '1'
+        }, 1500, function () {
+            $("#beard").delay(500).fadeIn(500);
+        });
+
+        
+    }
+
+});
